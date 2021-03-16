140ef9758ace864e7835790411a4486930ebcd3f
“itschristylin” <itschristylin@gmail.com>
2021-03-16 17:27:59 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var name = "Yiwen Lin";
+alert("Hello " + name);
+var name = prompt("請輸入你的名字");
+var age = 19;
+var isBoy = false;
+var fruits = ["apple","banana","peach"];
+alert(name); 
+alert(age); 
+alert(isBoy); 
+alert(fruits[2]); 
+function addiction(num){
+	return num+10;
+}
+var age = 28;
+var trueage= addiction(age);
+alert(trueage);
+
+var age2= 15;
+var trueage2= addiction(age2);
+alert(trueage2);
+function test(){
+a = 123;
+var b =456;
+if(a == 123) var c =789;
+}
+
+document.getElementsByTagName("div")[2].innerHTML="我來自Tag";
+document.getElementsByTagName("div")[2].style.fontsize="40px";
+document.getElementsByTagName("p")[0].innerHTML="abcdefg";
+document.getElementsByTagName("p")[0].style.fontsize="30px";
+document.getElementsByTagName("p")[0].style.color="red";
+
